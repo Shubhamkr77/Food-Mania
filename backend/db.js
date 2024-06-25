5d0d658c3f0c55c964088d7ef8a659dc1454889c
Shubhamkr77 <137918428+Shubhamkr77@users.noreply.github.com>
2024-06-25 16:48:48 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true); // if not want to retain the current behavior then do false
-const mongoURI =
-  "mongodb+srv://gofood:mern123@cluster0.9bsvovb.mongodb.net/gofoodmern?retryWrites=true&w=majority"; //mongodb server url
+const mongoURI = 'mongodb://<!#@!#@>:"!@!@"@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/Customer?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority' // Customer change url to your db you created in atlas
+// mongodb://<username>:<password>@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority
 const mongoDB = async () => {
   await mongoose.connect(
     mongoURI,
